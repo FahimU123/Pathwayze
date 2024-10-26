3ea31f9f0aa58ea067b6eec49d4b7593b54d8233
FahimU313 <115724162+FahimU313@users.noreply.github.com>
2024-10-26 15:04:35 -0400

    Initial Commit

---
+++ Pathwayze/PathwayzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathwayzeApp.swift
+//  Pathwayze
+//
+//  Created by Fahim Uddin on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct PathwayzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PathwayzeTests/PathwayzeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PathwayzeTests.swift
+//  PathwayzeTests
+//
+//  Created by Fahim Uddin on 10/26/24.
+//
+
+import Testing
+@testable import Pathwayze
+
+struct PathwayzeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
